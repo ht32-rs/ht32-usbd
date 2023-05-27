3be35f6446e2e790700b22b28b5f16a950cfbad0
Jonas Spanoghe <jonas@spanoghe.net>
2023-05-27 08:15:44 +0200

    Add doc-comment for `src/pac`.

--- src/pac/mod.rs
+++ src/pac/mod.rs
@@ -1,4 +1,12 @@
 //! Handcrafted PAC for the USB peripheral registers
+//!
+//! Here the endpoint registers have a *unified interface*, to allow a more elegant implementation
+//! of the [`UsbBus`](crate::bus::UsbBus).
+//!
+//! It is important to keep in mind that in reality not every endpoint accepts every field that has
+//! been defined here.
+//! For instance the *SETUP Data Received* interrupt is only applicable to endpoint 0,
+//! whereas the *Single-Buffering or Double-Buffering Selection* bit is only valid for endpoint 4-7.
 
 #[allow(unused, non_camel_case_types)]
 pub mod usb;
